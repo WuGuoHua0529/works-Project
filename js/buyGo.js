5cf58a03916daf26053a0e245d16f378ced68c20
wu82820 <hra82820@email.com>
2021-04-17 18:39:08 +0800

    fix : 修正buyGo問題

--- js/buyGo.js
+++ js/buyGo.js
@@ -129,15 +129,8 @@ function goCarts (data) {
     alert("加入完成!")
   }
 };
-function getCatsNum () {
-  const localData = localStorage.getItem('cartsData');
-  const dataLength = JSON.parse(localData).length;
-  $('.cars-now-num').empty().append(dataLength);
-};
 $(document).ready(function (){
-  getCatsNum()
   buyData.forEach(item => {
     showBuy(item)
   })
-  let cartsTime = window.setInterval("getCatsNum()",3000); 
 });
